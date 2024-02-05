d6e8543b398d05c2e47b0521c63b818192487252
huangzeting <huangzeting@yoyohulian.com>
2024-02-05 19:28:41 +0800

    feat: upgrade blog from cali.so

---
+++ app/layout.tsx
@@ -0,0 +1,89 @@
+import './globals.css'
+import './clerk.css'
+import './prism.css'
+
+import { ClerkProvider } from '@clerk/nextjs'
+import { type Metadata } from 'next'
+
+import { ThemeProvider } from '~/app/(main)/ThemeProvider'
+import { url } from '~/lib'
+import { zhCN } from '~/lib/clerkLocalizations'
+import { sansFont } from '~/lib/font'
+import { seo } from '~/lib/seo'
+
+export const metadata: Metadata = {
+  metadataBase: seo.url,
+  title: {
+    template: '%s | Cali Castle',
+    default: seo.title,
+  },
+  description: seo.description,
+  keywords: 'Cali,Cali Castle,郭晓楠,佐玩,创始人,CEO,开发者,设计师,细节控,创新',
+  themeColor: [
+    { media: '(prefers-color-scheme: dark)', color: '#000212' },
+    { media: '(prefers-color-scheme: light)', color: '#fafafa' },
+  ],
+  manifest: '/site.webmanifest',
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  openGraph: {
+    title: {
+      default: seo.title,
+      template: '%s | Cali Castle',
+    },
+    description: seo.description,
+    siteName: 'Cali Castle',
+    locale: 'zh_CN',
+    type: 'website',
+    url: 'https://cali.so',
+  },
+  twitter: {
+    site: '@thecalicastle',
+    creator: '@thecalicastle',
+    card: 'summary_large_image',
+    title: seo.title,
+    description: seo.description,
+  },
+  alternates: {
+    canonical: url('/'),
+    types: {
+      'application/rss+xml': [{ url: 'rss', title: 'RSS 订阅' }],
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider localization={zhCN}>
+      <html
+        lang="zh-CN"
+        className={`${sansFont.variable} m-0 h-full p-0 font-sans antialiased`}
+        suppressHydrationWarning
+      >
+        <body className="flex h-full flex-col">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
