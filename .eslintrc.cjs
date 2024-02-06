ecaa74fc01df9bf66645519411f0a6d1d0eeb3ac
huangzeting <huangzeting@yoyohulian.com>
2024-02-06 18:15:02 +0800

    feat: changed .eslintrc.js

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,7 @@ const config = {
         '@typescript-eslint/no-unsafe-call': 'off',
         '@typescript-eslint/no-unsafe-member-access': 'off',
         '@typescript-eslint/no-unsafe-assignment': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
       },
     },
   ],
