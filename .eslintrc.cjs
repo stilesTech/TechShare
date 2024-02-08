7cbdf3cbd2b5e32d14569c42bb7a1dfb9d00d6ea
huangzeting <huangzeting@yoyohulian.com>
2024-02-08 12:25:46 +0800

    change eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,7 +17,6 @@ const config = {
         '@typescript-eslint/no-unsafe-call': 'off',
         '@typescript-eslint/no-unsafe-member-access': 'off',
         '@typescript-eslint/no-unsafe-assignment': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
       },
     },
   ],
